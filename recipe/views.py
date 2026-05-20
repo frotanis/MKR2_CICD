e43d86b1262c663b2838d68fcbe87c3ac81bb527
frotanis <bubelkim12@gmail.com>
2026-05-20 17:54:55 +0300

    Add recipe_detail view

--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from .models import Recipe
+from .models import Recipe
 
 def main(request):
     # recipes_2023 = Recipe.objects.filter(created_at__year=2023)
@@ -8,3 +8,13 @@ def main(request):
         'recipes': []
     }
     return render(request, 'main.html', context)
+
+
+def recipe_detail(request, recipe_id):
+    # Шукаємо рецепт за його id. Якщо такого немає - видасть помилку 404
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+
+    context = {
+        'recipe': recipe
+    }
+    return render(request, 'recipe_detail.html', context)
